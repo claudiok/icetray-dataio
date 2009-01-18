539d655ddecee04b0f408bad02314c97780fe803
Troy Straszheim <troy@icecube.umd.edu>
2009-01-18 23:02:17 +0000

    I3LootFile -> I3SequentialFile
    modify I3Frame::merge, add I3Frame::purge
    
    tweak I3SequentialFile to not use specific dataclasses.
    
    Can't quite get rid of dataclasses in I3ReaderService, though, as the
    interface mentions I3Time.
    
    
    
    
    
    git-svn-id: http://code.icecube.wisc.edu/icetray/projects/dataio/trunk@36 579d543e-85e3-4578-a5e8-1e6a993b7602

--- public/dataio/I3File.h
+++ public/dataio/I3File.h
@@ -45,14 +45,11 @@ public:
   struct FrameInfo
   {
     I3Frame::Stream stream;
-    unsigned geo_frame;
-    unsigned cal_frame;
-    unsigned ds_frame;
+    typedef std::map<I3Frame::Stream, unsigned> stream_map_t;
+    stream_map_t other_streams;
 
     std::ios::pos_type pos;
-    FrameInfo() 
-      : stream(I3Frame::None), geo_frame(0), cal_frame(0), ds_frame(0) 
-    { }
+    FrameInfo() : stream(I3Frame::None) { }
   };
 
   I3File();
