bd5311c92bcfb5bf9a48f5f75fee39e15779dd8e
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-07-19 20:16:41 +0000

    Backport changes to QConverter to make WritePFrame work identically whether or not an I3EventHeader is present.
    git-svn-id: http://code.icecube.wisc.edu/icetray/projects/dataio/releases/q-frame-preview-3@808 579d543e-85e3-4578-a5e8-1e6a993b7602

--- private/dataio/QConverter.cxx
+++ private/dataio/QConverter.cxx
@@ -76,9 +76,13 @@ QConverter::Configure()
 
 void
 QConverter::Physics(I3FramePtr frame) {
-	// Purge any extraneous Q elements from this P frame left over
-	// from side effects of our subterfuge on I3Reader's frame cache
-	frame->purge(I3Frame::DAQ);
+	// Cache non P and non Q keys, to be merged back in later
+	I3FramePtr gcdcache(new I3Frame(*frame));
+	gcdcache->purge(I3Frame::DAQ);
+	gcdcache->purge(I3Frame::Physics);
+
+	// Purge all non-native keys, so we don't reassign GCD keys
+	frame->purge();
 
 	// Reassign special frame objects' stops from P -> Q
 	for (I3Frame::typename_iterator iter = frame->typename_begin();
@@ -94,13 +98,25 @@ QConverter::Physics(I3FramePtr frame) {
 			frame->ChangeStream(*iter, I3Frame::DAQ);
 	}
 
+	// If there is no event header, reassign all keys to the Q frame,
+	// since we can't make a stub P frame
+	if (!output_p_frame || !frame->Has("I3EventHeader")) {
+		for (I3Frame::typename_iterator iter = frame->typename_begin();
+		    iter != frame->typename_end(); iter++)
+			frame->ChangeStream(iter->first, I3Frame::DAQ);
+	}
+
+	// Merge all the GCD bits back in
+	frame->merge(*gcdcache);
+
 	// Clone GCDQ bits to a new Q frame
 	I3FramePtr daq(new I3Frame(*frame));
 	daq->SetStop(I3Frame::DAQ);
 	daq->purge(I3Frame::Physics);
 	PushFrame(daq);
 	
-	if(output_p_frame) {
+	// Write remnant keys, if they exist and it was requested, to a stub P
+	if(output_p_frame && frame->Has("I3EventHeader")) {
 		// Rewrite event header in P frame
 		I3EventHeaderPtr header(new
 		    I3EventHeader(frame->Get<I3EventHeader>()));
